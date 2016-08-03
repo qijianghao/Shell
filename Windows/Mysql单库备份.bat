de29f1a007164629d20cd185f9723c39211d7cf6
qijianghao <qijianghao@163.com>
2016-08-03 16:43:55 +0800

    备份脚本

--- Windows/Mysql单库备份.bat
+++ Windows/Mysql单库备份.bat
@@ -1,7 +1,9 @@
 rem 设置变量
-set Time=%date:~0,10%
-set MysqldumpPath=D:\xampp\mysql\bin\mysqldump.exe
+set Time=%date:~0,4%-%date:~5,2%-%date:~8,2%
+set MysqldumpPath=D:\mysql\bin\mysqldump.exe
 set Backpath=E:\backup\mysql
+
+rem 数据库名称 只支持单个数据库
 set Databases=DB_NAME
 set HOST="127.0.0.1"
 set USER=root
@@ -10,5 +12,5 @@ set PASSWD=PASSWD
 rem 关闭屏幕输出
 @echo off
 
-rem for循环备份多个数据库
-"%MysqldumpPath%" -h%HOST% --opt -u%USER% -p%PASSWD% %Databases% > %Backpath%\%Time%\%%D_%Time%.sql
+rem 备份数据库
+"%MysqldumpPath%" -h%HOST% --opt -u%USER% -p%PASSWD% %Databases% > %Backpath%\%Databases%_%Time%.sql
