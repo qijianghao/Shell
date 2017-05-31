249ec87cf8ad6f599883292d79bcf6c39e87e204
Starlight <qijianghao@163.com>
2017-05-31 17:24:14 +0800

    自动备份脚本

--- Linux/Shell备份脚本.sh
+++ Linux/Shell备份脚本.sh
@@ -1,60 +1,71 @@
-数据库备份
+1、数据库备份
 支持多库备份
 
 #!/bin/bash
-Databases=(db_Name db_Name2 db_Name3) 
+Databases=(db_Name1 db_Name2 db_Name3) 
 Backpath='/www/backup/db/'
 HOST="127.0.0.1"
 USER="root"
-PASSWD="PASSWD"
+PASSWD="123456"
 Time=`date +"%y-%m-%d"`
 mysqldump_path='/usr/bin/mysqldump'
-mkdir -p $Backpath$Time
+
 if [ ! -d "$Backpath" ]; then
   mkdir -p "$Backpath"
 fi
+
+mkdir -p $Backpath$Time
 for db in ${Databases[*]}
   do
     $mysqldump_path -h$HOST -u$USER -p$PASSWD $db|gzip > $Backpath$Time/"$db"_"$Time".sql.gz
   done
-#    cd  $Backpath$Time/
-#    tar zcf "$db"_"$Time".sql.tar.gz "$db"_"$Time".sql
-#    find $Backpath -mtime +60 -name "*.sql.gz" -exec rm -rf {} \;
-#     find $Bakdir -type d -empty -exec rmdir {} \; #删除空目录
 
-#All db
+    #保留60天备份文件
+    find $Backpath -mtime +60 -name "*.sql.gz" -exec rm -rf {} \;
+    #删除空目录
+    find $Bakdir -type d -empty -exec rmdir {} \;
+
+
+2、查询当前实例下所有数据，for循环备份
 
 #!/bin/bash
-
 Backpath='/backup/db/'
 HOST="127.0.0.1"
 USER="root"
-PASSWD="hongru123,.!"
+PASSWD="123456"
 Time=`date +"%y-%m-%d"`
 mysqldump_path='/usr/bin/mysqldump'
 Databases=`/usr/bin/mysql -u$USER -p$PASSWD -Bse 'show databases'`
-mkdir -p $Backpath$Time
+
 if [ ! -d "$Backpath" ]; then
   mkdir -p "$Backpath"
 fi
+
+mkdir -p $Backpath$Time
 for db in $Databases
   do
     $mysqldump_path -h$HOST -u$USER -p$PASSWD $db|gzip > $Backpath$Time/"$db"_"$Time".sql.gz
   done
 
-文件打包
+    #保留60天备份文件
+    find $Backpath -mtime +60 -name "*.sql.gz" -exec rm -rf {} \;
+    #删除空目录
+    find $Bakdir -type d -empty -exec rmdir {} \;
+
+
+3、文件打包
 
 #!/bin/bash
-Webdir=/www/website/
+Webdir=/www/website/abc.com
 Bakdir=/www/backup/web/
+Bakname=abc.com
 Date=`date +"%y-%m-%d"`
 mkdir -p $Bakdir/$Date
 cd $Bakdir/$Date
-tar zcf flynar.Manager-$Date.tar.gz $Webdir
+tar zcf $Bakname-$Date.tar.gz $Webdir
 
-tar -zcf /backup/web/www_`date +%Y%m%d`.tar.gz /www/web.com
 
-for循环打包，文件夹下子文件夹单独打包
+4、for循环打包，文件夹下子文件夹单独打包
 
 #!/bin/bash
 Webdir=/www/website/
@@ -68,15 +79,19 @@ for j in $BakdirName
 	do
 		tar zcf $j-$Date.tar.gz ${Webdir}/$j
 	done
-find $Bakdir -mtime +60 -name "*.tar.gz" -exec rm -rf {} \;
-find $Bakdir -type d -empty -exec rmdir {} \;
 
-异地ftp自动备份
+  #保留60天备份文件
+  find $Bakdir -mtime +60 -name "*.tar.gz" -exec rm -rf {} \;
+  #删除空目录
+  find $Bakdir -type d -empty -exec rmdir {} \;
+
+
+5、异地ftp自动备份
 #!/bin/bash
 Time=`date +"%y-%m-%d"`
-FTP_HOST="221.122.117.188:4242"
-FTP_USER="XHR_184"
-FTP_PASSWD="z5trs9xXBJe9ET"
+FTP_HOST="1.1.1.1"
+FTP_USER="UserNmae"
+FTP_PASSWD="123456"
 
 cd /backup/db
 lftp << EOF
@@ -99,7 +114,9 @@ bye
 EOF
 
 
-Crontb计划任务
+6、Crontb计划任务
+#每月1号2点开始备份网页文件
 0 2 1 * * /backup/webbak.sh 2>&1 >>/backup/webbak.log
+#每周一 1点开始备份数据库
 0 1 * * 1 /backup/dbbak.sh 2>&1 >>/backup/dbbak.log
 
--- Linux/auto_bak.sh
+++ Linux/auto_bak.sh
@@ -5,24 +5,26 @@ Databases=(DB_hlbn db_hualu2015 hualu2011 juyiwang)
 dbBakdir='/backup/db/'
 HOST="127.0.0.1"
 USER="root"
-PASSWD="weige123,.!"
+PASSWD="123456"
 Time=`date +"%y-%m-%d"`
 mysqldump_path='/usr/bin/mysqldump'
 
 #web环境变量配置
 Webdir=/var/www/html/
 webBakdir=/backup/web/
+Webname=abc.com
 
 #异地备份FTP参数配置
-FTP_HOST="221.122.117.188:4242"
-FTP_USER="bak_hlbn"
-FTP_PASSWD="JkDb9HE5Qy14"
+FTP_HOST="1.1.1.1:21"
+FTP_USER="Username"
+FTP_PASSWD="123456"
 
 #开始db备份，并清理90天前的历史备份
-mkdir -p $dbBakdir$Time
 if [ ! -d "$dbBakdir" ]; then
   mkdir -p "$dbBakdir"
 fi
+
+mkdir -p $dbBakdir$Time
 for db in ${Databases[*]}
   do
     $mysqldump_path -h$HOST -u$USER -p$PASSWD $db|gzip > $dbBakdir$Time/"$db"_"$Time".sql.gz
@@ -33,12 +35,12 @@ for db in ${Databases[*]}
 #开始web备份，并清理90天前的历史备份
 mkdir -p $webBakdir/$Time
 cd $webBakdir/$Time
-tar zcf hlbn-$Time.tar.gz $Webdir
+tar zcf $Webname-$Time.tar.gz $Webdir
 find $webBakdir -mtime +90 -name "*.tar.gz" -exec rm -rf {} \;
 find $webBakdir -type d -empty -exec rmdir {} \;
 
 #异地备份自动上传当天日期的备份文件夹
-cd /backup/db
+cd $dbBakdir
 lftp << EOF
 open $FTP_HOST
 user $FTP_USER $FTP_PASSWD
@@ -48,7 +50,7 @@ mirror -R $Time
 bye
 EOF
 
-cd /backup/web
+cd $webBakdir
 lftp << EOF
 open $FTP_HOST
 user $FTP_USER $FTP_PASSWD
