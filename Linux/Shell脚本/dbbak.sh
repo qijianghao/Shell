90269e910ee89b51ea1f73abd9628ab744242975
Starlight <qijianghao@163.com>
2017-05-31 17:55:38 +0800

    dbbak.sh

--- Linux/Shell脚本/dbbak.sh
+++ Linux/Shell脚本/dbbak.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 Databases=(dadifengjing) 
-Backpath='/home/backup/db/'
+Bakdir='/home/backup/db/'
 HOST="127.0.0.1"
 USER="abc"
 PASSWD="123456"
 Time=`date +"%y-%m-%d"`
 mysqldump_path='/www/wdlinux/mysql-5.5.54/bin/mysqldump'
 
-if [ ! -d "$Backpath" ]; then
-  mkdir -p "$Backpath"
+if [ ! -d "$Bakdir" ]; then
+  mkdir -p "$Bakdir"
 fi
 
-mkdir -p $Backpath$Time
+mkdir -p $Bakdir$Time
 for db in ${Databases[*]}
   do
-    $mysqldump_path -h$HOST -u$USER -p$PASSWD $db|gzip > $Backpath$Time/"$db"_"$Time".sql.gz
+    $mysqldump_path -h$HOST -u$USER -p$PASSWD $db|gzip > $Bakdir$Time/"$db"_"$Time".sql.gz
   done
 
     #保留60天备份文件
-    find $Backpath -mtime +60 -name "*.sql.gz" -exec rm -rf {} \;
+    find $Bakdir -mtime +60 -name "*.sql.gz" -exec rm -rf {} \;
     #删除空目录
     find $Bakdir -type d -empty -exec rmdir {} \;
