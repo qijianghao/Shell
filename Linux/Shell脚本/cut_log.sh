405e4fcbca4566db3a88177618f3758bfac20e1d
Starlight <qijianghao@163.com>
2017-06-02 11:03:06 +0800

    Updata

--- Linux/Shell脚本/cut_log.sh
+++ Linux/Shell脚本/cut_log.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #设置日志文件存放目录
-logs_path="/usr/local/nginx/logs/"
-mv ${logs_path}access.log ${logs_path}access_$(date -d "yesterday" +"%Y%m%d").log
-/usr/local/nginx/sbin/nginx -s reopen
+logs_path="/alidata/log/nginx/access"
+Nginx_path="/alidata/server/nginx-1.4.4/sbin"
+mv ${logs_path}/www_access.log ${logs_path}/www_access_$(date -d "yesterday" +"%Y%m%d").log
+$Nginx_path/nginx -s reopen
