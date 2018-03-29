553556c8a699aa6df449a2f5ef665cbe53523971
Starlight <qijianghao@163.com>
2018-03-29 15:13:35 +0800

    update zookeeper

--- Linux/安装脚本/install_zookeeper.sh
+++ Linux/安装脚本/install_zookeeper.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+if [ `rpm -qa|grep jdk|wc -l` -eq 0 ];then
+  if [ ! -f jdk-8u66-linux-x64.rpm ];then
+  wget -c http://image2.bjhongru.com/Software/Java/jdk-8u66-linux-x64.rpm
+  rpm -ivh jdk-8u66-linux-x64.rpm
+  echo '' >> /etc/profile
+  echo 'JAVA_HOME=/usr/java/latest/' >> /etc/profile
+  echo 'PATH=$JAVA_HOME/bin:$PATH' >> /etc/profile
+  echo 'CLASSPATH=.:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar' >> /etc/profile
+  echo 'export JAVA_HOME PATH CLASSPATH' >> /etc/profile
+  source /etc/profile
+    fi
+    fi
+
+
 if [ ! -f zookeeper-3.4.11.tar.gz ];then
 wget -c http://image2.bjhongru.com/Software/Java/zookeeper-3.4.11.tar.gz
 fi
