1f3d8c8a51c8a23fe11d0ded57b27bb167b980fb
qijianghao <qijianghao@163.com>
2019-06-03 16:51:51 +0800

    update 2019.6.3_2

---
+++ Linux/Shell脚本/mail_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+Time=`date +"%y-%m-%d %H:%M"`
+Mail_from=qijianghao@163
+Mail_passwd=wanqjh789.
+Mail_to=296985168@qq.com
+smtp_address=smtp.163.com
+bak_name=171_webbak_2018
+
+sendEmail -f qijianghao@163.com -t 296985168@qq.com -s smtp.163.com -xu qijianghao@163.com -xp wanqjh789. -u "$bak_name异地备份已完成。" -m "通知邮件: $bak_name 已在$Time 备份完成,请知晓."
