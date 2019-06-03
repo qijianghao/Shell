6af238e8e8060b6ce9dd2ebcc509dd11d1dde14f
qijianghao <qijianghao@163.com>
2019-06-03 16:47:05 +0800

    update 2019.6.3

--- Windows/ftp_sync.bat
+++ Windows/ftp_sync.bat
@@ -27,3 +27,27 @@ echo bye >>ftp.up
 echo quit >>ftp.up
 
 ftp -s:ftp.up
+
+rem 异地备份Mssql 数据库备份
+set sourcepath=D:\Backup\Mssql
+set destpath=192.168.1.7
+set dirname=Mssql
+
+@echo Off
+echo open %ftp_host% %ftp_port%>ftp.up
+echo %ftp_user%>>ftp.up
+echo %ftp_passwd%>>ftp.up
+echo prompt off>>ftp.up
+echo binary>>ftp.up
+echo lcd %sourcepath%>>ftp.up
+echo cd %destpath%>>ftp.up
+echo mkdir %dirname%>>ftp.up
+echo cd %dirname%>>ftp.up
+echo mkdir %time%>>ftp.up
+echo cd %time%>>ftp.up
+echo mput %time%\*>>ftp.up
+echo prompt on >>ftp.up
+echo bye >>ftp.up
+echo quit >>ftp.up
+
+ftp -s:ftp.up
